c0cc3ae7f6ea0f25a7ccd1d4394eb1463158e899
solomonovaa <sasha0193244@gmail.com>
2023-04-15 12:41:31 +0300

    MNotes
    MNotes

---
+++ MNotes/Notes/Notes/Views/FavoritePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class FavoritePage : ContentPage
+    {
+        public FavoritePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
